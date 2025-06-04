500019d5c3d33f83bbdac8899c8e8b1aa2a2dfbb
anny-tar <2201236@stud.samgups.ru>
2025-06-04 18:07:59 +0400

    Initial commit: Основная структура проекта

---
+++ library_project/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from library_project.views import HomePageView
+from stats.views import DashboardView
+from users.views import NoPermissionView, ProfileView
+
+urlpatterns = [
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('books/', include('books.urls')),
+    path('news/', include('news.urls')),
+    path('reviews/', include('reviews.urls')),
+    path('', HomePageView.as_view(), name='home'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    path('no-permission/', NoPermissionView.as_view(), name='no_permission'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
